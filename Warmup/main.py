c6e5e0a085afe02fb2cdc5314185f7f0f3567d75
Samuel Kim <samuelkim@college.harvard.edu>
2014-04-14 16:23:41 -0400

    Gamma=1

--- Warmup/main.py
+++ Warmup/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import policyIter as policy
 import plotDarts as plot
 
-gamma = 0.5
+gamma = 1
 
 pi = policy.policyIter(gamma)
 v = policy.solveV(pi, gamma)
@@ -10,3 +10,5 @@ v = policy.solveV(pi, gamma)
 print pi, v
 
 plot.bar(v)
+
+#85 has the highest value
